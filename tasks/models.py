c4a5ccead0d4d5bec0cfe6e9d13f291fd8f82f59
horus <carloch1234@gmail.com>
2024-01-12 12:38:35 -0500

    tarea a db y admin

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    fechacompletado = models.DateField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.titulo
