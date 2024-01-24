fecd482d4b3019abac7fe0572bd94667491479ed
horus <carloch1234@gmail.com>
2024-01-23 19:42:43 -0500

    ok falta logout pal desplegue

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Tarea
 
-class TareaForm(ModelForm):
+class TareaForm(forms.ModelForm):
     class Meta: 
         model = Tarea
         fields = ['titulo', 'descripcion', 'important',]
+        widgets = {'titulo' : forms.TextInput(attrs={'class': 'form-control', 'placeholder':'escribe titulo'}),
+            'descripcion' : forms.Textarea(attrs={'class': 'form-control','placeholder':'escribe titulo'}),
+            'important' : forms.CheckboxInput(attrs={'class': 'form-check-input, m-auto'})  
+        } #widget para atributo campos fields
         
